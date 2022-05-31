bb53159be0f9bb66ec21c179595be494ae9fd49e
Sinancancengiz <sinancan03@gmail.com>
2022-05-31 19:44:48 +0300

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     //
     public function index()
     {
-        echo "Index Function";
+        echo "Index Functionn";
     }
 
     public function test()
